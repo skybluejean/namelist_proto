f38c96f8fc3413f0d0fb22127ee8810b82472dd7
ellen <ellenjin51@gmail.com>
2023-12-12 13:50:12 +0900

    명함카드 박스 뷰 띄우기

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.css'; // CSS 파일을 import 합니다.
 
+/*
 const Card = () => {
   return (
     <div className="card">
@@ -13,5 +14,110 @@ const Card = () => {
     </div>
   );
 };
+*/
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <CardThumnail/>
+      <CardInfoName/>
+      <CardInfoJob/>
+      <CardContactPhone/>
+      <CardContactEmail/>
+    </div>
+  );
+};
+
+/*
+class Card extends React.Component{
+  render(){
+    return(
+      <div>
+        <CardThumnail image={this.props.image}/>
+        <CardInfo name={this.name.props} job={this.job.props}/>
+        <CardContactInfo phone={this.props.phone} email={this.props.email}/>
+      </div>
+    );
+  }
+}
+*/
+
+/*
+class CardInfo extends React.Component{
+  render(){
+    return(
+      <div className='card'>
+        <h2 className='name'>{this.props.name}</h2>
+        <p className='job-title'>{this.props.job}</p>
+      </div>
+    )
+  }
+}
+*/
+
+const CardInfoName = () =>{
+    return(
+      <div className='name'>
+        <h2>Jin Her</h2>
+      </div>
+    )
+}
+
+const CardInfoJob = () =>{
+  return(
+    <div>
+      <p className='job-title'>Developer</p>
+    </div>
+  )
+}
+
+/*
+class CardContactInfo extends React.Component{
+  render(){
+    return(
+      <div className='contact-info'>
+        <p>{this.props.phone}</p>
+        <p>{this.props.email}</p>
+      </div>
+    )
+  }
+}
+*/
+
+const CardContactPhone = () =>{
+  return(
+    <div className='contact-info'>
+        <p>Phone : 010-2627-7336</p>
+    </div>
+  )
+}
+
+const CardContactEmail = () =>{
+  return(
+    <div className='contact-info'>
+        <p>E-mail : herj@euclidsoft.co.kr</p>
+    </div>
+  )
+}
+
+/*
+class CardThumnail extends React.Component{
+  render(){
+      return(
+        <div>
+          <img src={this.props.image} alt="thumnail"/>
+        </div>
+      )
+  }
+}
+*/
+
+const CardThumnail = () =>{
+  return(
+   <div>
+    <img src='' alt="thumnail"/>
+  </div>
+  )
+}
 
 export default Card;
